cf40acd575347ecdd73a117fa169fdc087dce93d
Hasan Aktaş <aktashasan1130@gmail.com>
2022-11-10 13:21:05 +0300

    Update CalculationMethod.java

--- 2.week/leapyear-calculation/src/main/java/CalculationMethod.java
+++ 2.week/leapyear-calculation/src/main/java/CalculationMethod.java
@@ -1,19 +1,13 @@
 public class CalculationMethod {
 
-    public String calculation(Integer year){
+    public String calculation(Integer year) {
 
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0){
-                    return year+" artık bir yıldır.";
-                }else{
-                    return year+" artık bir yıl değildir.";
-                }
-            }else {
-                return year+" artık bir yıldır.";
-            }
-        } else{
-            return year+" artık bir yıl değildir.";
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            return year + " artık bir yıldır.";
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return year + " artık bir yıldır.";
+        } else {
+            return year + " artık bir yıl değildir.";
         }
     }
 }
