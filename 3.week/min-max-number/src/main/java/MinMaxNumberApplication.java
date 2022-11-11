40d82713df61cd2afa7e7a379a55c903d87d74e5
Hasan Aktaş <aktashasan1130@gmail.com>
2022-11-11 20:54:32 +0300

    min-max-added

---
+++ 3.week/min-max-number/src/main/java/MinMaxNumberApplication.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MinMaxNumberApplication {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int count = scanner.nextInt();
+
+        int number, min=1, max=1;
+
+        for (int i =1; i<=count;i++){
+            System.out.print(i + ". sayıyı giriniz:");
+            number= scanner.nextInt();
+
+            if(i==0){
+                min = number;
+                max = number;
+            }
+            if(number > max){
+                max = number;
+            }
+            if(number< min){
+                min = number;
+            }
+        }
+
+        System.out.println("En büyük sayı: " + max);
+        System.out.println("En küçük sayı: " + min);
+    }
+}
