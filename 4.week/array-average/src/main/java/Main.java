79211701c52a027458a45d227084608e277fdf03
Hasan Aktaş <aktashasan1130@gmail.com>
2022-11-13 14:11:17 +0300

    harmonic-average added

---
+++ 4.week/array-average/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        double sum = 0;
+
+        for (int i : numbers) {
+            sum += 1.0 / i;
+        }
+
+        System.out.println(numbers.length / sum);
+
+    }
+}
