5f0299568e5e5495b1609f2da0381e48edd96a20
Hasan Aktaş <aktashasan1130@gmail.com>
2022-11-13 11:31:42 +0300

    box-match added

---
+++ 4.week/box-match/src/main/java/Fighter.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.TimeUnit;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    public int hit(Fighter foe) throws InterruptedException {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " +  this.damage + " hasar vurdu.");
+
+        TimeUnit.SECONDS.sleep(1);
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+
+        if (foe.health - this.damage < 0)
+            return 0;
+
+        return foe.health - this.damage;
+    }
+
+    public boolean dodge() {
+        double randomValue = Math.random() * 100;  //0.0 to 99.9
+        return randomValue <= this.dodge;
+    }
+}
